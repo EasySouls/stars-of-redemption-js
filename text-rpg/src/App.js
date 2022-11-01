d10a49ca6a807dac146a09c13837e0b16e8aadcf
EasySouls <retek@DESKTOP-HTNKTMG>
2022-11-01 18:28:26 +0100

    Forms

--- text-rpg/src/App.js
+++ text-rpg/src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
 import Clock from "./Clock";
+// import { NameForm, EssayForm, FlavorForm, Reservation } from "./Forms";
 
 function App() {
-  return (
-    <div className='App'>
-      <Clock />
-    </div>
-  );
+  return <div className='App'></div>;
 }
 
 export default App;
