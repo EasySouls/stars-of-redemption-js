23523727d956b24f90fc10a10a57cf3b79661527
EasySouls <retek@DESKTOP-HTNKTMG>
2022-11-04 18:06:36 +0100

    todo: upgrade button

--- text-rpg/src/CharacterScreen.js
+++ text-rpg/src/CharacterScreen.js
@@ -6,7 +6,7 @@ export default function CharacterScreen(props) {
   const [canUpgrade, setCanUpgrade] = useState(false);
 
   useEffect(() => {
-    if (character.exp >= character.expNext) {
+    if (character.exp > character.expNext) {
       setCanUpgrade((prev) => true);
     }
   }, [character.exp, character.expNext]);
@@ -14,7 +14,32 @@ export default function CharacterScreen(props) {
   return (
     <div className='character-screen'>
       <h1>Character</h1>
-      <div className='character-screen-attributes'></div>
+      <div className='character-screen-attributes'>
+        <div className='attribute-item'>
+          Strength: {character.strength}{" "}
+          {canUpgrade ? <button>Upgrade</button> : null}
+        </div>
+        <div className='attribute-item'>
+          Dexterity: {character.dexterity}
+          {canUpgrade ? <button>Upgrade</button> : null}
+        </div>
+        <div className='attribute-item'>
+          Constitutuion: {character.constitutuion}
+          {canUpgrade ? <button>Upgrade</button> : null}
+        </div>
+        <div className='attribute-item'>
+          Intelligence: {character.intelligence}
+          {canUpgrade ? <button>Upgrade</button> : null}
+        </div>
+        <div className='attribute-item'>
+          Wisdom: {character.wisdom}
+          {canUpgrade ? <button>Upgrade</button> : null}
+        </div>
+        <div className='attribute-item'>
+          Charisma: {character.charisma}
+          {canUpgrade ? <button>Upgrade</button> : null}
+        </div>
+      </div>
     </div>
   );
 }
