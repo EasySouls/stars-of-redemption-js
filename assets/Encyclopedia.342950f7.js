734513e955be6b588a0623e82e30f2cb377af101
EasySouls <retek@DESKTOP-HTNKTMG>
2022-12-30 00:37:18 +0100

    Updates

--- assets/Encyclopedia.342950f7.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./index.4892d02c.js";function c(){return t("div",{children:t("h1",{className:"font-italic text-3xl font-bold",children:"Encyclopedia"})})}export{c as default};
---
+++ assets/Encyclopedia.f9849d53.js
@@ -0,0 +1,1 @@
+import{a as t}from"./index.efeed2a6.js";function c(){return t("div",{children:t("h1",{className:"font-italic text-3xl font-bold",children:"Encyclopedia"})})}export{c as default};
